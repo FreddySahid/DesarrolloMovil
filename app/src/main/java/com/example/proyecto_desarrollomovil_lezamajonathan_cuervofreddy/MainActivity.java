add31e87be0e5d895a9f8ea81f3f4bec683ce6e3
unknown <freddysahid@outlook.com>
2022-04-19 12:17:10 -0500

    Inicio del proyecto

---
+++ app/src/main/java/com/example/proyecto_desarrollomovil_lezamajonathan_cuervofreddy/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.proyecto_desarrollomovil_lezamajonathan_cuervofreddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
