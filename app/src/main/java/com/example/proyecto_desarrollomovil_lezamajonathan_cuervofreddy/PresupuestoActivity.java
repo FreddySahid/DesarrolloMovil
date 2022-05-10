3d017d36c05bceed8aaa249964e894bcf2b3b481
unknown <freddysahid@outlook.com>
2022-05-10 14:08:10 -0500

    Vista registrar presupuesto

--- app/src/main/java/com/example/proyecto_desarrollomovil_lezamajonathan_cuervofreddy/PresupuestoActivity.java
+++ app/src/main/java/com/example/proyecto_desarrollomovil_lezamajonathan_cuervofreddy/PresupuestoActivity.java
@@ -7,43 +7,18 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity {
-
-
-    private SQLiteService BD;
+public class PresupuestoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_presupuesto);
 
         Toolbar tolbar = findViewById(R.id.toolbarmenu);
         setSupportActionBar(tolbar);
-
-        BD = new SQLiteService(this);
-
-        ArrayList<Usuario> listaUsuario = BD.ConsultaUsuario();
-
-        ArrayList<String> listaUsuarioString = new ArrayList<String>(listaUsuario.size());
-        for(Usuario c: listaUsuario){
-            listaUsuarioString.add(c.getNombre() +"-"+ c.getEmail()+ " - "+ c.getPassword());
-        }
-
-        ArrayAdapter<String> adaptador =  new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listaUsuarioString);
-
-        ListView listaview = (ListView) findViewById(R.id.ListaUsuarios);
-        listaview.setAdapter(adaptador);
-
-
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -56,8 +31,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
         if(id == R.id.opcion1){
-            Intent intent = new Intent(MainActivity.this, PresupuestoActivity.class);
-            startActivity(intent);
+            Toast.makeText(this, "Ya se encuentra ahí.", Toast.LENGTH_LONG).show();
         }else if(id == R.id.opcion2){
             Toast.makeText(this, "Opción 2", Toast.LENGTH_LONG).show();
         }else if(id == R.id.opcion3){
@@ -65,7 +39,8 @@ public class MainActivity extends AppCompatActivity {
         }else if(id == R.id.opcion4){
             Toast.makeText(this, "Opción 4", Toast.LENGTH_LONG).show();
         }else if(id == R.id.opcion5){
-            Toast.makeText(this, "Ya se encuentra ahí.", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(PresupuestoActivity.this, MainActivity.class);
+            startActivity(intent);
         }
         return true;
     }
